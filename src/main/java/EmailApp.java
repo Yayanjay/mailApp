c627878fb0d01fd2bffdd09f5c67624a4cbada10
Yayanjay <yayanabdillah01@gmail.com>
2023-06-06 16:50:21 +0700

    update user input

--- src/main/java/EmailApp.java
+++ src/main/java/EmailApp.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class EmailApp {
     public static void main(String[] args) {
-        Email mail = new Email("Zayyan", "Abdillah");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the firstname : ");
+        String firstname = sc.next();
+        System.out.print("Enter the lastname : ");
+        String lastname = sc.next();
+
+        Email mail = new Email(firstname, lastname);
         System.out.println(mail.showInfo());
     }
 }
